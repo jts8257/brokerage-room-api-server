c911a7c70982ba3c765cb72b27409066f7a02407
jts8257 <k.jts8257@gmail.com>
2024-10-08 09:08:33 +0900

    refact : 같은 도메인의 역할을 수행하는 UserController 와 TokenController 통합

--- src/main/java/com/tsjeong/brokerage/controller/TokenController.java
+++ src/main/java/com/tsjeong/brokerage/controller/TokenController.java
@@ -3,32 +3,44 @@ package com.tsjeong.brokerage.controller;
 import com.tsjeong.brokerage.dto.ResponseDto;
 import com.tsjeong.brokerage.dto.token.TokenIssueResponse;
 import com.tsjeong.brokerage.dto.token.TokenIssueRequest;
+import com.tsjeong.brokerage.dto.user.UserReadResponse;
 import com.tsjeong.brokerage.service.token.issue.TokenIssueService;
+import com.tsjeong.brokerage.service.user.UserReadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/tokens")
 public class TokenController {
 
     private final TokenIssueService tokenIssueService;
+    private final UserReadService userReadService;
 
-    @PostMapping("/issue")
+
+    @GetMapping("/users")
+    public ResponseEntity<ResponseDto<List<UserReadResponse>>> getAllUsers() {
+
+        List<UserReadResponse> usersList = userReadService.getAllUsers();
+
+        return ResponseEntity.ok(ResponseDto.success(usersList));
+    }
+
+    @PostMapping("/tokens/issue")
     public ResponseEntity<ResponseDto<TokenIssueResponse>> issueToken(
             @Valid @RequestBody TokenIssueRequest requestBody
     ) {
-        return ResponseEntity.ok(
-                ResponseDto.success(
-                        tokenIssueService.issueTokenBy(requestBody.getEmail(), requestBody.getPassword())
-                )
-        );
+
+        TokenIssueResponse tokenIssueResponse = tokenIssueService.issueTokenBy(
+                requestBody.getEmail(), requestBody.getPassword());
+
+        return ResponseEntity.ok(ResponseDto.success(tokenIssueResponse));
 
     }
 }
