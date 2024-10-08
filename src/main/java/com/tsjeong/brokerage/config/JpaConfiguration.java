4b73e427d67c8eefd8cfeebb9e6605a3f846b296
jts8257 <k.jts8257@gmail.com>
2024-10-08 21:18:27 +0900

    refact : @EnableJpaAuditing 글로벌 설정 제거

--- src/main/java/com/tsjeong/brokerage/config/JpaConfiguration.java
+++ src/main/java/com/tsjeong/brokerage/config/JpaConfiguration.java
@@ -3,12 +3,14 @@ package com.tsjeong.brokerage.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Configuration
-public class QueryDslConfig {
+@EnableJpaAuditing
+public class JpaConfiguration {
 
     @PersistenceContext
     private EntityManager entityManager;
